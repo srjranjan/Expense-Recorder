b69ca9a0c8f51191100867289e8fd61e03b64fdc
srjranjan <sudhanshuwithyoualll@gmail.com>
2020-06-29 19:09:40 +0530

    ---

--- app/src/main/java/com/srj/expenserecorder/activity/AddExpense.kt
+++ app/src/main/java/com/srj/expenserecorder/activity/AddExpense.kt
@@ -18,8 +18,8 @@ class AddExpense : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_expense)
-
         setDate()
+
 
 
         btn_save.setOnClickListener {
